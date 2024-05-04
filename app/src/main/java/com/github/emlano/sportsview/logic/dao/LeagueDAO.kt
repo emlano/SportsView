3dd98212d198bdfb2c299a2d2b49d17eaa2906a4
emilano <emlano.git@gmail.com>
2024-05-04 17:55:16 +0530

    feat: Added LeagueDAO class

--- app/src/main/java/com/github/emlano/sportsview/logic/dao/LeagueDAO.kt
+++ app/src/main/java/com/github/emlano/sportsview/logic/dao/LeagueDAO.kt
@@ -1,8 +1,27 @@
 package com.github.emlano.sportsview.logic.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.github.emlano.sportsview.logic.entity.League
 
 @Dao
 interface LeagueDAO {
+    @Query("SELECT * FROM League")
+    suspend fun getAllLeagues(): List<League>
 
+    @Query("SELECT * FROM League WHERE id = :id")
+    suspend fun getLeague(id: Int): League
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addLeague(league: League)
+
+    @Update
+    suspend fun updateLeague(league: League)
+
+    @Delete
+    suspend fun deleteLeague(league: League): Int
 }
